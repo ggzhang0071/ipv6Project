fda9ffcd8e0bc9fd833575f25f60627ab7f87cbc
ggzhang0071 <34293109+ggzhang0071@users.noreply.github.com>
2017-12-20 04:40:21 -0500

    main脚本

--- Nlp/Learnbgame/learnbgame_pyscript/learnbgame.py
+++ Nlp/Learnbgame/learnbgame_pyscript/learnbgame.py
@@ -48,17 +48,4 @@ if __name__ == '__main__':
 
 
 
-#peepdf:Python 编写的PDF文件分析工具，可以帮助检测恶意的PDF文件
-
-#Didier Stevens' PDF tools:析，识别和创建 PDF 文件(包含PDFiD，pdf-parser，make-pdf和 mPDF)
-
-#Opaf: 开放 PDF 分析框架，可以将 PDF 转化为 XML 树从而进行分析和修改
-
-#Origapy: Ruby 工具Origami的 Python 接口，用于审查 PDF 文件
-
-#pyPDF2: Python PDF 工具包包含：信息提取，拆分，合并，制作，加密和解密等等
-
-#PDFMiner:从 PDF 文件中提取文本
-
-#python-poppler-qt4: Python 写的 Poppler PDF 库，支持 Qt4
 
